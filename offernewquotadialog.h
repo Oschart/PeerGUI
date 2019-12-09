ed3d4a5ece1c110c3b2646cd6188c3745e5a5e34
Fadi <fadiadel@aucegypt.edu>
2019-12-09 12:25:16 +0200

    setQuota(tobecont)

---
+++ offernewquotadialog.h
@@ -0,0 +1,25 @@
+#ifndef OFFERNEWQUOTADIALOG_H
+#define OFFERNEWQUOTADIALOG_H
+
+#include <QDialog>
+using namespace std;
+
+namespace Ui {
+class OfferNewQuotaDialog;
+}
+
+class OfferNewQuotaDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit OfferNewQuotaDialog(string, string, QWidget *parent = nullptr);
+    ~OfferNewQuotaDialog();
+
+private:
+    Ui::OfferNewQuotaDialog *ui;
+    string user;
+    string imageName;
+};
+
+#endif // OFFERNEWQUOTADIALOG_H
